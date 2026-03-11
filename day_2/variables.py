826568238979e35b05eb51d054ca05bc00f9cb26
sousa-24 <diogofsousapt@gmail.com>
2026-03-11 18:18:32 +0000

    Day 2: variables, types, operators, input

---
+++ day_2/variables.py
@@ -0,0 +1,57 @@
+# Day 2: 30 Days of python programming
+
+# Exercise: lvl1
+name = "Diogo"
+last_name = "Sousa"
+full_name = "Diogo Sousa"
+country = "Portugal"
+city = "Porto" 
+age = 28
+year = 2026
+is_married = False
+is_true= True
+is_light_on = True
+pet,car = "Buddy","Ford Focus"
+
+# Exercise: lvl2
+print(type(name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(pet))
+print(type(car))
+
+print(len(name))
+print(len(last_name)-len(name))
+
+num_one= 5
+num_two= 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+floor_division = num_one // num_two
+
+area_of_circle = 3.14 **2 * 30
+circumference_of_circle = 2 * 3.14 * 30
+
+radius = int(input("Enter the radius of a circle: "))
+area_of_circle = 3.14 **2 * radius
+print("The area of the circle is: ", area_of_circle)
+
+name = input("What is your name? ")
+print("Hello " + name)
+last_name = input("What is your last name? ")
+print("Your full name is " + name + " " + last_name)
+country = input("Where are you from? ")
+print("You are from " + country)
+age = input("How old are you? ")
+print("You are " + age + " years old")
+
